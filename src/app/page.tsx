d3c1f6a7fcd83e3ab488c44472757720512ef0ec
aniruddha <aniruddhathakur76@gmail.com>
2024-07-16 11:39:24 +0530

    Added padding to entire bento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Github from "@/components/Github";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-dvh">
+    <div className="flex justify-center items-center min-h-dvh p-4">
       <Github />
     </div>
   );
